36546a63d468d861d85a0293b8bac13d06df9f1f
mauleenn <mauleen.ndlovu@outlook.com>
2021-02-23 10:22:03 -0600

    Added medicationModel -- model class that holds the data structure for each medication

---
+++ HarmonyMood/Models/medicationModel.swift
@@ -0,0 +1,15 @@
+//
+//  medicationModel.swift
+//  SQLite_Database
+//  HarmonyMood
+//
+//  Created by Mauleen Ndlovu on 2/22/21.
+//
+
+import Foundation
+
+class medicationModel: Identifiable {
+    public var medID: Int64 = 0
+    public var name: String = ""
+    public var dosage: Int64 = 0
+}
