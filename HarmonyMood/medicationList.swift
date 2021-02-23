0f60a1051a3a704311f61cc11c01ee0a3be82fd1
mauleenn <mauleen.ndlovu@outlook.com>
2021-02-23 10:20:02 -0600

    Users can add new medications, edit, and delete them with data persistence

--- HarmonyMood/medicationList.swift
+++ HarmonyMood/medicationList.swift
@@ -23,48 +23,93 @@
 import SwiftUI
 import Combine
 
+/*
 struct Medications: Hashable {
 
     let name: String
     let dosage: String
     
 }
+*/
 
 struct medicationList: View {
     
-    @State private var listMedications = [Medications]()
+ //   @State private var listMedications = [Medications]()
     
-    @State private var medicationName: String = ""
-    @State private var medicationDosage: String = ""
-    @State private var medicationUnits: String = ""
+    // Array of medication models
+    @State var medicationModels: [medicationModel] = []
+    
+    // check if medication is selected for edit
+    @State var selectedMed: Bool = false
+     
+    // id of selected medication to edit or delete
+    @State var selectedMedID: Int64 = 0
+
 
     @State private var medicationStartingDate = Date()
     @State private var medicationTimeofDay: Int = 0
-    @State private var mgUnit: Int = 0
 
     private var timeOfDayOptions = ["🌞 AM", "🌙 PM", "🌓 Both"]
-    private var medicationUnit = ["mg"]
  
     @State private var addMedicationButton = false
     
+    @State var name: String = ""
+    @State var units: String = ""
+    @State var dosage: String = ""
+     
+    // to go back on the home screen when the user is added
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+     
     
     var body: some View {
         ZStack() {
                 VStack {
+                    VStack {
+                        // navigation link to go to edit user view
+                        NavigationLink (destination: editMedicationView(id: self.$selectedMedID), isActive: self.$selectedMed) {
+                            EmptyView()
+                        }
+                    }
                     NavigationView {
-                        List {
-                            ForEach(listMedications, id: \.self) { medication in
-                                Section(header: Text("")) {
-                                    
-                                    HStack {
-                                        VStack(alignment: .leading) {
-                                            Text(medication.name);
-                                            Text(medication.dosage).font(.subheadline).foregroundColor(.gray)
-                                        }
-                                    }
+                        List (self.medicationModels) { (meds) in
+                            Section(header: Text("")) {
+                            HStack {
+                                VStack (alignment: .leading) {
+                                Text(meds.name)
+                                    Text("\(meds.dosage) mg").font(.subheadline).foregroundColor(.gray)
                                 }
+                                
+                                // Button to edit medication
+                                Button(action: {
+                                    
+                                   self.selectedMedID = meds.medID
+                                    self.selectedMed = true
+                                    
+                                }, label: {
+                                    Image(systemName: "pencil")
+                                        .foregroundColor(.black)
+                                        .font(.system(size: 33.0))
+                                    
+                                }).buttonStyle(PlainButtonStyle())
+                                
+                            
+                                // Button that allows user to delete an exisiting medication
+                                Button(action: {
+                                    
+                                    // create db manager instance
+                                    let dbManager: DB_Manager = DB_Manager()
+                                    
+                                    // call delete function
+                                    dbManager.deleteMedication(idValue: meds.medID)
+                                    self.medicationModels = dbManager.getMeds()
+                                    
+                                    
+                                }) {
+                                    Text("➖")
+                                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                }.buttonStyle(PlainButtonStyle())
                             }
-                            .onDelete(perform: delete)
+                            }
                         }
                         .listStyle(InsetGroupedListStyle())
                         .navigationBarTitle(Text("Medications List"))
@@ -76,46 +121,34 @@ struct medicationList: View {
                                                     Text("➕")
                                                         .font(.title)
                                                 })
-                                                    // Button that allows user to delete an exisiting medication
-                                                    Button(action: {
-                                                        EditButton()
-                                                    }) {
-                                                        Text("➖")
-                                                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                                                    }
                                                 },
                                             trailing:
                                                     // Link to get to the "Welcome to HarmonyMood" Page
                                                     NavigationLink(destination: infoPage()) {
                                                         Text("ℹ️")
                                                             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                                                    }
-                                                    
-                                                )
-                    }.sheet(isPresented: $addMedicationButton) {
+                                                    })
+                    }
+                    .onAppear(perform: {
+                        self.medicationModels = DB_Manager().getMeds()
+                    })
+                    .sheet(isPresented: $addMedicationButton) {
                         Form {
                             // Medication name
                             Section(header: Text("Information")) {
                             HStack {
                                 Text("Medication Name: ")
-                                TextField("", text: self.$medicationName)
+                                TextField("", text: self.$name)
                                 
                             }
                               
                             // Medication dosage
                             HStack {
                                 Text("Dosage: ")
-                                TextField("", text: self.$medicationDosage)
+                                TextField("", text: self.$dosage)
                                     .keyboardType(.numberPad)
                             }
                             
-                            // Medication unit is mg
-                            Picker("Units", selection: $mgUnit) {
-                                ForEach(0..<medicationUnit.count) {
-                                    Text(self.medicationUnit[$0])
-                                }
-                            }
-                                
                             // Medication can be taken in the morning, evening, or both
                             Picker("Time of Day", selection: $medicationTimeofDay) {
                                 ForEach(0..<timeOfDayOptions.count) {
@@ -127,24 +160,31 @@ struct medicationList: View {
                             // Starting date of medication
                            Section(header: Text("Starting Date")) {
                                DatePicker(selection: $medicationStartingDate, in: ...Date(), displayedComponents: .date) {
-                                   Text("Date")
+                                   Text("Date: ")
                                }
                            }
                             // "Add" button
                             Section {
-                            Button(action: {
-                                self.listMedications.append(Medications(name: self.medicationName, dosage: self.medicationDosage))
-                                self.addMedicationButton.toggle()
+                                Button(action: {
                                 
-                                print(self.listMedications)
-                                
-                                // Reset Values
-                                 self.medicationName = ""
-                                self.medicationDosage = ""
-                            }, label: {
-                                Text("Add")
-                            })
+                                    // call function to add row in sqlite database
+                                    DB_Manager().addMedication(nameValue: self.name, unitsValue: self.units, dosageValue: Int64(self.dosage) ?? 0)
+                                     
+                                   // self.listMedications.append(Medications(name: self.name, dosage: self.dosage))
+                                    self.addMedicationButton.toggle()
+                                    
+                                 //   print(self.listMedications)
+                                    
+                                    // Reset Values
+                                     self.name = ""
+                                    self.dosage = ""
+                                }, label: {
+                                    Text("Add")
+                                })
                             }
+                            .onAppear(perform: {
+                                self.medicationModels = DB_Manager().getMeds()
+                            })
                     } // End of Form
             } // End of .sheet
                     
@@ -153,11 +193,6 @@ struct medicationList: View {
     }
         
 }
-    
-    // Allows users to swipe to delete a medication
-    func delete(at offsets: IndexSet) {
-        listMedications.remove(atOffsets: offsets)
-    }
     
 struct medicationList_Previews: PreviewProvider {
     static var previews: some View {
